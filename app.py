68659bc75a803ddca66920c4a6142e33695deb94
TheTuli <rt3991@g.rit.edu>
2021-03-25 00:03:44 -0400

    travis

--- app.py
+++ app.py
@@ -10,16 +10,26 @@ location = st.text_input('Enter your location?')
 
 
 def get_temp(location):
+    params, query_url = get_url_and_params(location)
+    response_body = requests.get(url=query_url, params=params)
+    return extract_temperature(response_body.json())
+
+
+def extract_temperature(response_body):
+    temperature = response_body['main']['temp']
+    return temperature
+
+
+def get_url_and_params(location):
     query_url = f"{url}?q={location}&appid={ACCESS_KEY}"
     params = {
         'units': 'imperial'
     }
-    r = requests.get(url=query_url, params=params)
-    temperature = r.json()['main']['temperature']
-    return temperature
+    return params, query_url
 
 
 if location:
     temp = get_temp(location)
-    st.write(f"{location} weather:")
-    st.write(f" {temp} Fahrenheit")
+    st.write(f"{location} weather (Fahrenheit):")
+    st.write(temp)
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,20 @@
-def test_get_temp():
-    assert True
+from app import extract_temperature
+
+
+def test_extract_temperature_when_valid_body():
+    test_response_body = {
+        'main': {'temp': 60}
+    }
+    assert extract_temperature(test_response_body) == 60
+
+
+def test_key_error_in_extract_temperature_when_invalid_body():
+    test_response_body = {
+        'main': {}
+    }
+    try:
+        extract_temperature(test_response_body)
+        assert False
+    except KeyError:
+        assert True
+
