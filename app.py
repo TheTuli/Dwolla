0529910697c60065628e80fd879eb2eb30ae3e2a
TheTuli <rt3991@g.rit.edu>
2021-03-24 23:11:46 -0400

    test structure

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ st.title("Rahul's Technical Exercise")
 
 location = st.text_input('Enter your location?')
 
-if location:
-    query_url = f"{url}?q={location}&appid={ACCESS_KEY}"
 
+def get_temp(location):
+    query_url = f"{url}?q={location}&appid={ACCESS_KEY}"
     params = {
         'units': 'imperial'
     }
     r = requests.get(url=query_url, params=params)
+    temperature = r.json()['main']['temperature']
+    return temperature
+
+
+if location:
+    temp = get_temp(location)
     st.write(f"{location} weather:")
-    temp = r.json()['main']['temp']
-    st.write(f"{temp} degrees Fahrenheit")
-
-
+    st.write(f" {temp} Fahrenheit")
